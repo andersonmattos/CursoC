a4b7496436a33fb06d27455bc9b5f73503df43ca
Anderson Mattos <andersonaraujo.mattos@gmail.com>
2021-02-10 19:43:42 -0300

    Teste do push

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -8,6 +8,7 @@ namespace TesteGit
         {            
             Console.WriteLine("Teste Git");
             Console.WriteLine("Aulta Udemy");
+            Console.WriteLine("Teste Push");
         }
     }
 }
